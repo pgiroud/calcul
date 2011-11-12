f1772782619ee6e9c8715a9199d8e4fbb4b82afe
Patrick Giroud <patrick.giroud@laposte.net>
2011-11-12 19:12:07 +0100

    Montant d'impôt arrondi au franc

--- src/test/java/ch/ge/afc/calcul/impot/france/TestCalculImpotBaremeFrance.java
+++ src/test/java/ch/ge/afc/calcul/impot/france/TestCalculImpotBaremeFrance.java
@@ -14,15 +14,15 @@ public class TestCalculImpotBaremeFrance {
 	public void calcul2007() {
 		FournisseurFrance fournisseur = new FournisseurFrance();
 		Bareme bareme = fournisseur.getBaremeRevenu(2007);
-		assertEquals("Impôt 2007 pour revenu de 45000",new BigDecimal("8191"),bareme.calcul(new BigDecimal("45000")));
-		assertEquals("Impôt 2007 pour revenu de 320611",new BigDecimal("116181"),bareme.calcul(new BigDecimal("320611")));
+		assertEquals("Impôt 2007 pour revenu de 45000",new BigDecimal("8191.00"),bareme.calcul(new BigDecimal("45000")));
+		assertEquals("Impôt 2007 pour revenu de 320611",new BigDecimal("116181.00"),bareme.calcul(new BigDecimal("320611")));
 	}
 
 	@Test
 	public void calcul2008() {
 		FournisseurFrance fournisseur = new FournisseurFrance();
 		Bareme bareme = fournisseur.getBaremeRevenu(2008);
-		assertEquals("Impôt 2008 pour revenu de 26200",new BigDecimal("2397"),bareme.calcul(new BigDecimal("26200")));
+		assertEquals("Impôt 2008 pour revenu de 26200",new BigDecimal("2397.00"),bareme.calcul(new BigDecimal("26200")));
 	}
 
 }
