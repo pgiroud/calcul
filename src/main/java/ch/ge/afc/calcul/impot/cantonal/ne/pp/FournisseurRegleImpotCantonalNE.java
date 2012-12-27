3d1af89cca9ef2bf02a84e08fa43ba0b943f5ca4
Patrick Giroud <patrick.giroud@laposte.net>
2012-12-27 21:09:13 +0100

    Changement de répertoire de base

--- src/main/java/ch/ge/afc/calcul/impot/cantonal/ne/pp/FournisseurRegleImpotCantonalNE.java
+++
@@ -1,8 +0,0 @@
-package ch.ge.afc.calcul.impot.cantonal.ne.pp;
-
-import ch.ge.afc.bareme.Bareme;
-
-public interface FournisseurRegleImpotCantonalNE {
-	Bareme getBaremeRevenu(int annee);
-	Bareme getBaremeFortune(int annee);
-}
--- src/main/java/ch/ge/afc/calcul/impot/taxation/is/ITypeMatiereImposable.java
+++
@@ -1,5 +0,0 @@
-package ch.ge.afc.calcul.impot.taxation.is;
-
-public interface ITypeMatiereImposable {
-	String getLibelle(String codeLangue);
-}
