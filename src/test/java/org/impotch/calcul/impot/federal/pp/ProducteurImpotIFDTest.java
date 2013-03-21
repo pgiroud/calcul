fba322787236b4da43a9696651510b67d8294553
Patrick Giroud <patrick.giroud@laposte.net>
2013-03-21 21:00:28 +0100

    Ajout classes de test sur production IFD avec rabais enfant

---
+++ src/test/java/org/impotch/calcul/impot/federal/pp/ProducteurImpotIFDTest.java
@@ -0,0 +1,67 @@
+package org.impotch.calcul.impot.federal.pp;
+
+import org.impotch.calcul.impot.Impot;
+import org.impotch.calcul.impot.federal.FournisseurRegleImpotFederal;
+import org.impotch.calcul.impot.taxation.pp.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/beans.xml")
+public class ProducteurImpotIFDTest extends ProducteurImpotTst {
+
+
+    @Resource(name = "fournisseurRegleImpotFederal")
+    private FournisseurRegleImpotFederal constructeur;
+    private Map<String,String> libelleImpotTaxe = new HashMap<String,String>();
+
+    public ProducteurImpotIFDTest() {
+        libelleImpotTaxe.put("IBR","Impôt fédéral de base sur le revenu");
+        libelleImpotTaxe.put("RI","rabais sur l'impôt de base");
+    }
+
+    private RecepteurMultipleImpot recepteur(String... codesImpot) {
+        RecepteurMultipleImpot recepteur = new RecepteurMultipleImpot();
+        for (String code : codesImpot) {
+            recepteur.ajouteRecepteur(code, new RecepteurUniqueImpot(code));
+        }
+        recepteur.ajouteRecepteur("TOTAL",new RecepteurImpotSomme());
+        return recepteur;
+    }
+
+    private BigDecimal getValeur(RecepteurMultipleImpot recepteur, String code) {
+        if ("TOTAL".equals(code)) {
+            return ((RecepteurImpotSomme)recepteur.getRecepteur(code)).getValeur();
+        } else {
+            Impot valeur = ((RecepteurUniqueImpot)recepteur.getRecepteur(code)).getValeur();
+            if (null == valeur) return null;
+            return valeur.getMontant();
+        }
+    }
+
+    private void verifierMontantImpot(RecepteurMultipleImpot recepteur, String codeImpotTaxe, String montantAttendu) {
+        assertEquals(codeImpotTaxe + " : " + libelleImpotTaxe.get(codeImpotTaxe),new BigDecimal(montantAttendu),getValeur(recepteur,codeImpotTaxe));
+    }
+
+
+    @Test
+    public void produireImpotIFD() {
+        ProducteurImpot prod = constructeur.getProducteurImpotsFederauxPP(2013);
+        RecepteurMultipleImpot recepteur = recepteur("IBR","RI");
+        prod.produireImpot(this.creerSituationFamilleAvecEnfant(12), this.creerAssiettes(2013, 52400), recepteur);
+        verifierMontantImpot(recepteur,"IBR",  "256.00");
+        verifierMontantImpot(recepteur,"RI", "-251.00");
+        verifierMontantImpot(recepteur,"TOTAL","5.00");
+    }
+
+
+}
