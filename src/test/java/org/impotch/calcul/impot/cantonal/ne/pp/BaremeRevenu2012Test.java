9637dc3dd8b09c0c95017296d104392755b782fe
Patrick Giroud <patrick.giroud@laposte.net>
2013-05-07 21:35:56 +0200

    Ajout test impôt revenu Neuchâtel

---
+++ src/test/java/org/impotch/calcul/impot/cantonal/ne/pp/BaremeRevenu2012Test.java
@@ -0,0 +1,47 @@
+/**
+ * This file is part of impotch/calcul.
+ *
+ * impotch/calcul is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * impotch/calcul is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with impotch/calcul.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.impotch.calcul.impot.cantonal.ne.pp;
+
+import org.impotch.bareme.Bareme;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/beans.xml")
+public class BaremeRevenu2012Test {
+
+    @Resource(name = "fournisseurRegleImpotCantonalNE")
+    private FournisseurRegleImpotCantonalNE fournisseur;
+
+    private Bareme bareme;
+
+    @Before
+    public void setUp() throws Exception {
+        bareme = fournisseur.getBaremeRevenu(2012);
+    }
+
+    @Test
+    public void celibataire() {
+        assertThat(bareme.calcul(BigDecimal.valueOf(100000))).isEqualTo("12300.00");
+    }
+}
